4e911a9abb9c1555ad39e015bc4f47db87355a63
velozerick <veloxer25@outlook.com>
2025-11-04 18:30:45 -0600

    variables

---
+++ 1_Variables/variables.py
@@ -0,0 +1,60 @@
+####################################################### VARIABLES EN PYTHON
+
+"""
+x = 8
+y = "Erick"
+
+print(x)
+print(y)
+
+
+
+
+
+x = 4   # x is of type int 
+
+x = "Salvador" # x is now type str
+print(x)
+
+"""
+
+###################################################### CONVERSION DE TIPOS DE DATOS
+x = str(3) # x will be '3'
+print(x)
+
+y = int(3) # y will be 3
+print(y)
+
+z = float(3) # z will be 3.0
+print(z)
+
+
+
+
+# ######################################################### OBTENER TIPO DE DATO
+
+a = 5
+u = 'Groot'
+
+print(type(a))
+print(type(u))
+
+f = 5.5
+print(type(f))
+
+
+
+
+
+
+# ######################################################### NOMBRES DE VARIABLES
+
+myvar = "Obi wan"
+my_var = "obi wan"
+_my_var = "obi wan"
+myVar = "Anakin"
+MYVAR = 'Padme'
+myvar3 = "C-3PO"
+
+print(myvar)
+
--- Hello there/hello.py
+++ Hello there/hello.py
@@ -1,3 +1,31 @@
 print("Hello there")
-
 print("General Kenobi")
+
+
+print("I am learning python")
+
+print('It is awesome')
+
+
+
+print(3)
+print(5000)
+
+print(3 + 5)
+print(2 * 5)
+
+
+print("I am", 22, "years old")
+
+
+# This is a comment
+print("That was a comment")
+
+
+"""
+This is a 
+multi-line comment
+thats crazy!
+"""
+
+
