85ab260002c81557507c9641df1e6035ea390c1c
velozerick <veloxer25@outlook.com>
2025-11-07 18:18:44 -0600

    variable exercises

---
+++ 1_Variables/var_ejer.py
@@ -0,0 +1,28 @@
+nombre = 'Erick'
+edad = 22
+altura = 1.71
+es_estudiante = True
+
+print(type(nombre))
+print(type(edad))
+print(type(altura))
+print(type(es_estudiante))
+
+
+#Mostrar variables en pantalla 
+
+print("Hola, mi nombre es", nombre, "y tengo", edad,"años")
+
+#Cambiar valor de variable
+
+edad = 50 # ahora edad es 50
+print (edad)
+
+
+
+NombreUsuario = input("Ingresa tu nombre: ")
+
+EdadUsuario = input("Ingresa tu edad: ")
+
+print("Hola", NombreUsuario, "tienes", EdadUsuario, "años")
+
