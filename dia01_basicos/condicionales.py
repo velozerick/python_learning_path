623af7d20a103c11aba2953405439890b93c406e
velozerick <veloxer25@outlook.com>
2026-01-19 15:26:17 -0600

    Dia 01 basicos (input, if, tipos de datos)

---
+++ dia01_basicos/condicionales.py
@@ -0,0 +1,9 @@
+nombre = input('¿Como te llamas? ')
+edad = int(input('¿Que edad tienes?' ))
+
+print('Hola ',nombre)
+
+if edad >= 18:
+	print("Eres mayor de edad")
+else:
+	print("Eres menor de edad no puedes estar aqui")
---
+++ dia01_basicos/main.py
@@ -0,0 +1,19 @@
+print("Dia 01 > Basicos entorno listo")
+
+nombre = "Erick"
+edad = 22
+altura = 1.71
+tiene_servidor = True
+
+print("Nombre: ",nombre)
+print("edad: ",edad)
+print("altura ",altura)
+print("tiene servidor ",tiene_servidor)
+
+# INPUTS
+
+nombre = input("¿Como te llamas? ")
+edad = input('¿Que edad tienes?')
+
+print('Un gusto',nombre)
+print('Tienes',edad,'años')
