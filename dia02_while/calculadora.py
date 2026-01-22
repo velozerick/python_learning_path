320651cbd2304ba7feebf8dab9ff2f9af79a9933
velozerick <veloxer25@outlook.com>
2026-01-21 18:14:12 -0600

    calculdora completa

--- dia02_while/calculadora.py
+++ dia02_while/calculadora.py
@@ -11,8 +11,14 @@ while opcion == 'si':
 	
 	if operacion == "suma":
 		print("El resultado es ", num1 + num2)
+	elif operacion == "resta":
+		print("El resultado es ", num1 - num2)
+	elif operacion == "multiplicacion":
+		print("El resultado es ", num1 * num2)
+	elif operacion == "division":
+		print("El resultado es ",num1 / num2)
 	else:
-		print('prueba lista')
+		print("Elige suma, resta, multiplicacion o division")
 	
 	opcion = input('Deseas continuar? (si/no)')
 print('Hasta luego')
