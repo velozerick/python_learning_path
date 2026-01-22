6638842d559086237fed7afd945d2a8d7be468b1
velozerick <veloxer25@outlook.com>
2026-01-22 12:55:05 -0600

    optimizacion de validacion

--- dia02_while/validacion_entrada.py
+++ dia02_while/validacion_entrada.py
@@ -1,3 +1,4 @@
+"""
 print('Ingrese contraseña para acceder')
 
 cont = "si"
@@ -11,5 +12,26 @@ while cont == "si":
 		print("Contraseña incorrecta")
 
 	cont = input("Desea volver a intentar? (si/no)")
+
+print("Fin del programa")
+"""
+
+# OPTIMIZACION DE CODIGO 
+
+print ('Ingrese contraseña para acceder') 
+
+password_correcta = "veloz3r2505" # declarar variable para contraseña correcta
+continuar = "si" # declarar variable para while
+
+while continuar == "si": # inincio de while
+	password_ingresada = input("Escriba la contraseña: ") # pedir a ususario escribir contraseña
+	
+	if password_ingresada == password_correcta: # verificacion de contraseña
+		print('Contraseña correcta puede continuar')
+		continuar = "no" # Termina el while 
+	else:
+		print("Contraseña incorrecta")
+	
+		continuar = input("Desea volver a intentar? (si/no): ").strip().lower() # preguntar al ususario si desea volver a intentar con el uso de strip y lower sin importar mayusculas o minusculas en la entrada
 
 print("Fin del programa")
