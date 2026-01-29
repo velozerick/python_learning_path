87a23cbe8c10ebba32a83a28b23a63e9f8a116cc
velozerick <veloxer25@outlook.com>
2026-01-29 11:28:15 -0600

    comentarios

--- dia03_while_full/while.py
+++ dia03_while_full/while.py
@@ -7,6 +7,6 @@ while contador <= 5: # mientras el contador sea menor o igual a 5
     print("Contador", contador) # desde el valor asignadoa la variable se sumara uno hasta que se romap la condicion  
     contador = contador + 1
 
-print("Fin del while")
+print("Fin del while") # fin del programa
 
 
