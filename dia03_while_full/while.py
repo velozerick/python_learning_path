e615651c0a47c517d90c039bd7f9626e3783300f
velozerick <veloxer@25outlook.com>
2026-01-29 11:23:37 -0600

    comentarios

--- dia03_while_full/while.py
+++ dia03_while_full/while.py
@@ -1,10 +1,10 @@
 # Dia 03 while repaso 
 #  Objetivo: entender como funciona while sin break ni continue
 
-contador = 1
+contador = 1 # crear variable contador con el valor de 1 
 
-while contador <= 5:
-    print("Contador", contador)
+while contador <= 5: # mientras el contador sea menor o igual a 5
+    print("Contador", contador) # desde el valor asignadoa la variable se sumara uno hasta que se romap la condicion  
     contador = contador + 1
 
 print("Fin del while")
