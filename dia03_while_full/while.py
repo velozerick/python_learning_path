270e7352c9684392e0ee0374625561deaf9d5a98
velozerick <veloxer@25outlook.com>
2026-01-29 11:20:16 -0600

    repaso while

---
+++ dia03_while_full/while.py
@@ -0,0 +1,12 @@
+# Dia 03 while repaso 
+#  Objetivo: entender como funciona while sin break ni continue
+
+contador = 1
+
+while contador <= 5:
+    print("Contador", contador)
+    contador = contador + 1
+
+print("Fin del while")
+
+
