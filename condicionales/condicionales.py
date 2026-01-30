fefb2e2fe503dc4e8465fec97e4d322319f092fb
velozerick <veloxer25@outlook.com>
2026-01-30 14:26:03 -0600

    ejercicio

---
+++ condicionales/condicionales.py
@@ -0,0 +1,20 @@
+
+my_condition = int(input("Escribe un numero del 1 al 20: "))
+
+if my_condition < 5:
+    print("La condicion es menor 5")
+
+
+if my_condition > 10 and my_condition < 20:
+    print("Es mayor que 10 y menor que 20")
+
+# ELIF 
+elif my_condition == 1:
+    print("Es igual a 1")
+
+else:
+    print("Es menor o igual que 10 o mayor o igual que 20")
+
+
+
+
