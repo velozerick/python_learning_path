5abfb2ae1b4f6b28b79cc8f7b1e89818bf11cf93
velozerick <veloxer25@outlook.com>
2026-01-29 19:48:35 -0600

    trabajo con vim

---
+++ dia03_while_full/condicionales/condicionales.py
@@ -0,0 +1,11 @@
+### CONDICIONALES ###
+
+# establecer flujos de ejecucion en codigo 
+
+my_condition = True
+
+if my_condition:
+    print("Se ejecuta la condicion del if") #imprime el mensaje
+else:
+	print("jaja es falso")
+    
