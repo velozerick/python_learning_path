046ff83d50cca71edd981f7c8167dc9abac55b90
velozerick <veloxer25@outlook.com>
2026-01-30 12:46:59 -0600

    continuacion de condicionales

--- dia03_while_full/condicionales/condicionales.py
+++ dia03_while_full/condicionales/condicionales.py
@@ -4,7 +4,18 @@
 
 my_condition = True
 
-if my_condition:
+if my_condition: # Es lo mmismo que mi condicion = True:
     print("Se ejecuta la condicion del if") #imprime el mensaje
-else:
-	print("jaja es falso")   
+
+
+my_condition = 5 * 3
+
+if my_condition == 10:
+	print('Se ejecuta la siguiente condicion')
+if my_condition > 7:
+	print("La tercera condicion se cumple")
+
+if my_condition > 10:
+	print("Es mayor que 10")
+else: 
+	print("Es menor o igual a 10")
