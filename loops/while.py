4e4d169f7070ac92561b54eb85587fa8124cc3ce
velozerick <veloxer25@outlook.com>
2026-01-30 15:20:02 -0600

    ejercicio con while

---
+++ loops/while.py
@@ -0,0 +1,28 @@
+### WHILE ###
+
+'''
+my_condition = 0
+
+while my_condition < 10:  # mientras que mi vriable sea menor que 10 , siempre imprimira "my_condition" hasta que explote el ordenador
+
+    print(my_condition)
+    my_condition += 1 # se le suma 1 a la variable hasta que llegue al valor 10 y por eso detiene el ciclo
+
+print('La ejecucion continua')
+
+'''
+
+my_cond = int(input('Escribe un numero :'))
+
+while my_cond < 10:
+    print(my_cond)
+    my_cond += 1
+
+if my_cond == 10:
+    print('El numero es igual a 10')
+
+else:
+    print('El numero ', my_cond,' es mayor o igual que 10')
+
+
+  
