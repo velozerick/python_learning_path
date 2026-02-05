fbd06cbfd41beeb0cd8d5ea6eb63b1f6dc217620
velozerick <veloxer25@outlook.com>
2026-02-05 08:32:45 -0600

    for loop

---
+++ loops/for/for.py
@@ -0,0 +1,45 @@
+'''
+For sirve para recorrer una coleccion de datos , uno por uno, 
+no repite x veces , es iterar sobre algo que y existe 
+
+una lista
+tupla 
+diccionario
+set
+string
+
+el while yo controlo el esado, decido cuadno termina
+y el for python controla el avance y yo solo decido que recorrer
+
+
+usaremos range()
+
+es algo que genera numeros uno por uno 
+
+range(5)  genera 0 1 2 3 4
+range (1, 4) genera 1 2 3
+
+'''
+
+'''
+for variable in algo:
+bloque 
+'''
+
+
+
+for i in range(5):
+    if i ==3:
+        print('llegue al 3')
+    else:
+        print('valor:', i)
+
+
+
+for numero in range(10):
+    if numero % 2 == 0:
+        print(numero, 'par')
+    else:
+        print(numero, 'impar')
+
+
