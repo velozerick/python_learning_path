53e83c6240534f973fec90572d4bcd469b10fb24
velozerick <veloxer25@outlook.com>
2026-02-06 09:02:24 -0600

    ejercicio for

--- loops/for/for.py
+++ loops/for/for.py
@@ -70,3 +70,29 @@ for bre in range(1 ,11):
     if bre == 7:
         print('llegue al 7, paro')
         break
+
+
+
+#for contador
+print('Ejercicio contador')
+contador = 0
+
+for i in range(1, 11):
+    if i % 2 ==0:
+        contador = contador + 1
+print(contador)
+
+
+
+# for control de acceso
+
+# En un sistema , los usuarios tienen IDs del 1 al 15, los usuarios con ID multiplo de 3 tienen acceso permitido, cuantos usuarios tienen acceso
+
+print('ejercicio acceso')
+
+usuarios = 0
+
+for uid in range(1, 16):
+    if uid % 3 == 0:
+        usuarios = usuarios + 1
+print(usuarios)
