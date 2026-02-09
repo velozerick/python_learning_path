0e8a128752bf3deb4b93e4f65c4b7faae9c934e9
velozerick <veloxer25@outlook.com>
2026-02-09 15:10:14 -0600

    retos diccionario

--- Diccionarios/Dictionary.py
+++ Diccionarios/Dictionary.py
@@ -32,3 +32,48 @@ print(persona)
 #Agregar
 persona['pais'] = 'México'
 print(persona)
+
+#Como saber si una clave existe?
+if "edad" in persona:
+    print('la clave edad existe')
+else:
+    print('la clave edad no existe')
+
+###########################################################
+
+
+#Diccionario ejemplo
+
+ejemplo = {
+    "avion": 'airbus',
+    'modelo': 320,
+    'destinos': ['cancun','puerto vallarta','Ciudad de méxico']
+
+}
+
+
+
+
+#Reto 1 ,imprimir primer destino
+
+print(ejemplo['destinos'][0])
+
+
+#Reto 2 mostrar destinos 
+
+for viaje in ejemplo['destinos']:
+        print('Destino disponible', viaje )
+
+
+#Reto 3 agregar una clave llamada "Internacional" imprimir si es verdadero imprime vuelo internacional , si es falso , vuelo nacional
+
+ejemplo['Internacional'] = False # Agregar clave y valor
+
+print(ejemplo)
+
+# si es Internacional 
+if ejemplo['Internacional'] == True:
+     print('vuelo internacional')
+     #si es nacional
+else:
+     print('Vuelo nacional')
