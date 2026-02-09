7526f7779ad572bcf85af1fceb3dc346c29a0f43
velozerick <veloxer25@outlook.com>
2026-02-09 14:26:10 -0600

    Adicion del porcentaje en control de siembra

--- sets/control de siembra.py
+++ sets/control de siembra.py
@@ -32,3 +32,13 @@ print(rechazados_unicos)
 
 print(c_aprobados)
 print(c_rechazados)
+
+
+# Calcular pocentaje de aprobados y rechazados 
+
+porcentaje_a = (c_aprobados / len(solicitudes) ) * 100 # crear variable de porcentaje, = al contador de aprobados entre el total de elementos , multiplicando por cien 
+print('EL porcentaje de aprobados es: ',porcentaje_a, '%') 
+
+# Calcular el procentaje de rechazados , ceando la variable del porcenaje de rechazados = al contador de rechazados entre el total de elementos , multiplicando por cien 
+porcentaje_r = (c_rechazados / len(solicitudes)) * 100
+print('EL porcentaje de rehazados es: ',porcentaje_r, '%')
