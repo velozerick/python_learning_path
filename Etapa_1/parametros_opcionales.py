8e15986a416948034f8acd96aa9c6ce5e0e136dd
velozerick <veloxer25@outlook.com>
2026-02-11 14:02:53 -0600

    descuento

--- Etapa_1/parametros_opcionales.py
+++ Etapa_1/parametros_opcionales.py
@@ -42,3 +42,24 @@ def calcular_precio_seguro(precio, iva = 0.16):
 resultado = calcular_precio_seguro(20)
 
 print(resultado)
+
+
+
+# precio descuento
+
+#crear funcion con parametros precio y descuento
+def calcular_descuento(precio, descuento = 0):
+    if precio < 0:
+        return 'precio invalido, ingrese nuevamente'
+    elif descuento < 0:
+        return 'Descuento invalido'
+    elif descuento > 1:
+        descuento = descuento / 100
+    else:
+        precio_descuento = precio * (1 - descuento)
+    return precio_descuento
+
+res_descuento = calcular_descuento(100,0.2)
+
+print('El precio con descuento es:',res_descuento)
+
