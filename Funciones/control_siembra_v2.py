618c9946578d3c50c8e16ee04f1a813cb059a918
velozerick <veloxer25@outlook.com>
2026-02-11 11:00:16 -0600

    control de siembra v2

--- Funciones/control_siembra_v2.py
+++ Funciones/control_siembra_v2.py
@@ -29,18 +29,47 @@ def normalizar_solicitudes(solicitudes): #crear la funcion con el parametro soli
     return normalizada#funcion termina y la manda al exterior
 lista_normalizada = normalizar_solicitudes(solicitudes)# guardar en una variable el resultado
 
-print(lista_normalizada)
+
 
 
 #clasificar solicitudes
 def clasificar_solicitudes(lista_normalizada,permitidos): #Crear la funcion con los parametros de la lista ya filtrada y la permitidos que es con la que vamos a sacar aprobados y rechazados
     lista_permitidos=[]#crear lista permitidos
+    lista_rechazados=[]#crear lista rechazados
     for c in lista_normalizada:#crear for, para lista_ normalizada en permitidos
         if c in permitidos: #si los elementos de lista normalizada esta en la lista permitidos
             lista_permitidos.append(c)
-    return lista_permitidos
+        else:
+            lista_rechazados.append(c)
+    return lista_permitidos, lista_rechazados
 
-lpermitidos = clasificar_solicitudes(lista_normalizada,permitidos)
-print(lpermitidos)
+lista_permitidos,lista_rechazados = clasificar_solicitudes(lista_normalizada,permitidos) #variable creada para el resultado
 
 
+#Resumen
+
+def resumen (lista_permitidos,lista_rechazados):#crear funcion donde las entradas son las listas creadas
+    #crear contadores, contador1 , contador2 uno para permitidos y otro para rechazados
+    contador_p = len(lista_permitidos)
+    contador_r = len(lista_rechazados)
+    total = contador_p + contador_r
+    #calcular el porcentaje parte/total * 100 de permitidos y de rechazados
+    porcentaje_p = len(lista_permitidos)/total * 100
+    porcentaje_r = len(lista_rechazados)/total * 100
+    #enviar con return
+    return porcentaje_p,porcentaje_r,contador_p,contador_r,total
+
+porcentaje_p,porcentaje_r,contador_p,contador_r,total = resumen(lista_permitidos,lista_rechazados)
+
+
+
+
+print('Los cultivos permitidos son:',lista_permitidos,'\n')
+print('Los cultivos rechazados son:',lista_rechazados,'\n')
+
+print('La cantidad de cultivos permitidos son:',contador_p,'\n')
+print('La cantidad de cultivos rechazados son:',contador_r,'\n')
+print('el total de cultivos son:',total)
+
+print('EL porcentaje de permitidos:',round(porcentaje_p,2),'%')
+print('El porcentaje de rechazados:',round(porcentaje_r,2),'%')
