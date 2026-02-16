5fbc69ddee108aaa3113873bdb881dde01a1d40c
velozerick <veloxer25@outlook.com>
2026-02-16 13:40:48 -0600

    pure vs effects

---
+++ Etapa_1/pure_vs_side_effects.py
@@ -0,0 +1,58 @@
+"""
+Demostracion de funciones puras vs funciones con efectos secundarios
+"""
+
+#PURE    mismas entradas mismas salidas
+#Sirve para transformar datos, calcular, validar y devolver resultados
+
+#SIDE EFFECTS
+# sirve para registrar, guardar, notificar, persisitir: por ejemplo guarda el email en una lista global o imprime un log
+
+LOGS = [] # variable global , se modifica desde afuera 
+
+#----------
+#FUNCION PURA
+#----------
+
+def validar_usuario(username, email):
+    #transformacion interna
+    username_limpio = username.strip()
+    email_limpio = email.strip().lower()
+
+    errores = [] # lista local 
+
+    #validaciones
+    if len(username_limpio) < 3:
+        errores.append("El username debe tener al menos 3 caracteres")
+
+    if "@"not in email_limpio:
+        errores.append("Email invalido")
+
+    # si hay errores termina aqui
+    if errores:
+        return False, None, errores
+    
+    #si todo esat bien -> crear diccionario
+
+    usuario = {
+        "username": username_limpio,
+        "email": email_limpio,
+        "activo": True
+    }
+
+    return True, usuario,[]
+
+
+#EFECTO SECUNDARIO
+
+def registrar_errores(errores):
+    for error in errores:
+        LOGS.append(error) #esto modifica algo externo
+
+ok,usuario,errores = validar_usuario("Erick","erickcorreo.com")
+
+if not ok:
+    registrar_errores(errores)
+else:
+    print("Usuario valido:" , usuario)
+print("logs",LOGS)
