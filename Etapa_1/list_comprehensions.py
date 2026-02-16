b165ae25c735fbb026b381b304095521b55e3f8e
velozerick <veloxer25@outlook.com>
2026-02-16 11:51:57 -0600

    ejercicio 2

--- Etapa_1/list_comprehensions.py
+++ Etapa_1/list_comprehensions.py
@@ -35,6 +35,41 @@ print(pares)
 
 print("\n Ejercicio 1\n")
 #Ejercicio 1
-nombres = ["Erick","Alejandra","Luis","Ignacio"]
-result = [name.lower() for name in nombres ]
+nombres = ["Erick","Alejandra","Luis","Ignacio"] #Crear la lista de nombres 
+result = [name.lower() for name in nombres ] # crear lista result , por cada name quiero generar su version en minusculas, la expresion es = que quiero contruir con cada elemento
 print(result)
+# de cierta manera se puede leer asi , primero se lee for despues condiciones y al final la expresion 
+
+
+
+
+
+
+
+print("\n ------Ejercicio 2------ \n")
+#Ejercicio 2 
+
+
+usuarios = [
+    {"username": "erick", "email": "erick@mail.com", "activo": True},
+    {"username": "ana", "email": "ana@mail.com", "activo": False},
+    {"username": "luis", "email": "luis@mail.com", "activo": True},
+    {"username": "maria", "email": "maria@mail.com", "activo": False},
+]
+
+
+def obtener_usuarios_activos(usuarios):
+    '''
+    user_act = [user for user in usuarios if user["activo"] == True ]
+    return user_act
+    '''
+    return [user["username"].upper() for user in usuarios if user["activo"]]
+user_lis = obtener_usuarios_activos(usuarios)
+print("Los usuarios activos son:",user_lis)
+
+
+def obtener_email(usuarios):
+    return [user["email"].lower() for user in usuarios if not user["activo"]]
+email_list = obtener_email(usuarios)
+print ("Email de los usuarios no activos: ", email_list)
+
