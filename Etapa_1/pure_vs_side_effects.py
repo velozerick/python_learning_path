a7f26eb028f2eb1294d32015cb2aafce561bfe9c
velozerick <veloxer25@outlook.com>
2026-02-16 14:31:30 -0600

    validacion de contraseña

--- Etapa_1/pure_vs_side_effects.py
+++ Etapa_1/pure_vs_side_effects.py
@@ -56,3 +56,67 @@ if not ok:
 else:
     print("Usuario valido:" , usuario)
 print("logs",LOGS)
+
+
+
+
+
+
+##########################################################################3
+
+#----------
+#Ejercicio
+#----------
+print("\n Validacion de contraseña \n")
+#Funcion pura
+
+#Crear lista Logs[]
+Logs = []
+
+#fucion validar_password
+
+def validar_password(password):
+    #filtrar entradas
+    clean_password = password.strip()
+
+    #crear lista errores
+    errores =[]
+
+    #validaciones
+    if len(clean_password) < 8:
+        errores.append("La contraseña debe tener minimo 8 caracteres")
+    
+    if not any(n.isdigit()for n in clean_password):
+            errores.append("La contraseña debe tener al menos un número")
+        #si hay errores termina aqui
+    if errores:
+            return False, None, errores
+
+#si todo esta bien 
+    return True, clean_password, errores
+
+
+#EFECTOS
+
+def registro_errores(errores):
+    for error in errores:
+        Logs.append(error)
+
+
+ok,clean_password,errores = validar_password(" hola1234")
+
+if not ok:
+    registro_errores(errores)
+else:
+    print(clean_password,"Contraseña correcta")
+print("LOGS",Logs)
+        
+    
+        
+    
+    
+
+
+
+
+
