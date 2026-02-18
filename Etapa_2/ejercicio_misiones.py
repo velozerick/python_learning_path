c2dc0403282926b4aba7bb88eb3cb5438d388a4a
velozerick <veloxer25@outlook.com>
2026-02-18 10:15:41 -0600

    ejercicio misiones

--- Etapa_2/ejercicio_misiones.py
+++ Etapa_2/ejercicio_misiones.py
@@ -20,3 +20,27 @@ print("Las misiones activas son : ",misiones_activas)
 
 
 # devolver un diccionario solo con las misiones activas y su destino
+
+def mission_info ():
+    list_mission = []
+    with open("misiones.txt", "r") as archivo: 
+        content = archivo.readlines()
+        for line in content:
+            partes = line.strip().split(":")
+            if partes[0] == "Mision":
+                nombre = partes[1]
+            if partes[0] == "Destino":
+                destino = partes[1]
+            if partes[0] == "Estado" and partes[1] == " activa":
+                estado = partes[1]
+                info = {
+                        "nombre": nombre,
+                        "destino": destino,
+                        "estado": estado
+                       }
+                list_mission.append(info)
+    return list_mission
+
+
+info_mision = mission_info()
+print("Informacion de las misiones : ",info_mision , "\n")
